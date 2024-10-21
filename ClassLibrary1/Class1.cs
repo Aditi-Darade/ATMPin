f8a6b30f44f0b424ddb57125d79be872db68bedb
Aditi <aditidarade1@gmail.com>
2024-10-21 13:22:45 -0500

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class DebitCard
+    {
+        public string pin;
+        public string Pin
+        {
+            set
+            {
+                if (value != "")
+                {
+
+                    if (value.Length == 4 || value.Length == 6)
+                    {
+                        for (int i = 0; i < value.Length; i++)
+                        {
+                            if (value[i] < 48 || value[i] > 57) //ASCII codes 48 to 57 are equal to digits (0 to 9)
+                            {
+                                System.Console.WriteLine("Pin cannot have alphabets, please enter a Pin with only digits.");
+                                break;
+                            }
+                            else
+                            {
+                                pin = value;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("The Pin should be exactly 4 digits or 6 digits.");
+                    }
+
+                }
+                else
+                {
+                    System.Console.WriteLine("Please enter the PIN.");
+                }                
+            }
+            get
+            {
+                return pin;
+            }
+        }
+    }
+}
---
+++ Sec9_Assign24_ATMPin/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec9_Assign24_ATMPin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DebitCard card = new DebitCard();
+            System.Console.WriteLine(card.Pin = "1y56");
+            //card.Pin = 9999;
+            System.Console.ReadKey();
+        }
+    }
+}
